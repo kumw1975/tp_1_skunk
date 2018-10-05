0367474bd9361db8622256fa6e29777d98225594
kumw1975 <kumw1975@stthomas.edu>
2018-10-04 21:18:15 -0500

    "Dice Class constructor Design"

--- SkunkProject/src/Dice.java
+++ SkunkProject/src/Dice.java
@@ -1,26 +1,30 @@
 import edu.princeton.cs.introcs.StdOut;
-
 /**
  * Dice represents a single pair of rollable Die objects, randomly generating
- * sums of their two values
- * 
- * This is a Javadoc comment: add more to your finished class below
- * 
- * @author eric
+ * sums of their two values * 
+ * This is a Javadoc comment: add more to your finished class below * 
+ * @authors Eric Level, Hughbert Kumwesiga & Wasswa Derrick
  *
  */
  
-public class Dice
-{
+public class Dice{
 	// Instance fields (variables) may be declared anywhere in class body
 	// Convention: put at top
-
-	private int lastRoll;
 	private Die die1;
 	private Die die2;
+	private int rollValue;
 
 	// Constructors (object initializers) also can be declared anywhere
 	// Convention: after instance fields/variables
+	// initialize instance variables die1 and die2 by
+	// creating a new instance of each
+	
+	// overloaded constructor :: use for testable die
+	public Dice(Die die1, Die die2){
+		this.die1 = new Die();
+		this.die2 = new Die();
+	}
+	
 
 	public Dice()
 	{
@@ -31,19 +35,14 @@ public class Dice
 		this.die2 = new Die();
 		this.roll();
 	}
-
-	public Dice(Die die1, Die die2) // overloaded constructor
-	{
-		this.die1 = die1;
-		this.die2 = die2;
-	}
+ 
 
 	// Instance methods can also be declared anywhere
 	// Convention: after constructors
 
 	public int getLastRoll()
 	{
-		return this.lastRoll;
+		 return 0;
 	}
 
 	public void roll()
@@ -53,7 +52,7 @@ public class Dice
 
 		die1.roll();
 		die2.roll();
-		this.lastRoll = die1.getLastRoll() + die2.getLastRoll();
+		//this.lastRoll = die1.getLastRoll() + die2.getLastRoll();
 
 	}
 
