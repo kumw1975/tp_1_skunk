163079cf80c4682c19f7ad91a92df84f65c72e7b
kumw1975 <kumw1975@stthomas.edu>
2018-10-04 22:14:17 -0500

    "Added Junit Tests that need to be implemented for the skunk app"

---
+++ SkunkProject/test/Game__Tests.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Game__Tests {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testGame() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetPlayers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetPlayers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testStartRound() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testStartTurn() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testPlay() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddChipsToKitty() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetLastRound() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ SkunkProject/test/Player__Tests.java
@@ -0,0 +1,231 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author kumw1975
+ *
+ */
+public class Player__Tests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Player#roll()}.
+	 */
+	@Test
+	public final void testRoll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#Player(java.lang.String)}.
+	 */
+	@Test
+	public final void testPlayerString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#Player(java.lang.String, int[], int[])}.
+	 */
+	@Test
+	public final void testPlayerStringIntArrayIntArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#getPlayerNumber()}.
+	 */
+	@Test
+	public final void testGetPlayerNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#setPlayerNumber(int)}.
+	 */
+	@Test
+	public final void testSetPlayerNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#setNumberOfChips(int)}.
+	 */
+	@Test
+	public final void testSetNumberOfChips() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#getNumberOfChips()}.
+	 */
+	@Test
+	public final void testGetNumberOfChips() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#getName()}.
+	 */
+	@Test
+	public final void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#getTurnPoints()}.
+	 */
+	@Test
+	public final void testGetTurnPoints() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#setTurnPoints(int)}.
+	 */
+	@Test
+	public final void testSetTurnPoints() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#getRoundPoints()}.
+	 */
+	@Test
+	public final void testGetRoundPoints() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#setRoundPoints(int)}.
+	 */
+	@Test
+	public final void testSetRoundPoints() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#getGamePoints()}.
+	 */
+	@Test
+	public final void testGetGamePoints() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#getDoubleSkunkCount()}.
+	 */
+	@Test
+	public final void testGetDoubleSkunkCount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#setDoubleSkunkCount(int)}.
+	 */
+	@Test
+	public final void testSetDoubleSkunkCount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#getTotalTurnsTaken()}.
+	 */
+	@Test
+	public final void testGetTotalTurnsTaken() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#incrementTotalTurnsTaken()}.
+	 */
+	@Test
+	public final void testIncrementTotalTurnsTaken() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#getTurnsTakenInCurrentRound()}.
+	 */
+	@Test
+	public final void testGetTurnsTakenInCurrentRound() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#setGamePoints(int)}.
+	 */
+	@Test
+	public final void testSetGamePoints() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#setTurnsTakenInCurrentRound(int)}.
+	 */
+	@Test
+	public final void testSetTurnsTakenInCurrentRound() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#takeNumberOfChips(int)}.
+	 */
+	@Test
+	public final void testTakeNumberOfChips() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Player#setPreviousMetrics()}.
+	 */
+	@Test
+	public final void testSetPreviousMetrics() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
